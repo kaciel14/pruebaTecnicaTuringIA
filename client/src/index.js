fad8efd91f4989b382b41d30c329efecd0ec1204
Kaciel Benitez <kacielbenitez@gmail.com>
2024-06-14 01:28:43 -0600

    m

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,7 @@ import { AuthenticationProvider } from './AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthenticationProvider>
     <App />
-  </AuthenticationProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
