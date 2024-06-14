36272857900e845015c4ccee0096793ebf7eb7b3
Kaciel Benitez <kacielbenitez@gmail.com>
2024-06-13 20:09:23 -0600

    v1.5

--- server/server/databaseConnection.js
+++ server/server/databaseConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2')
+const {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE} = require('./config')
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    port:'3306',
-    user: 'root',
-    password: 'superuser',
-    database: 'pruebaTecnica'
+    host: DB_HOST,
+    port:DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE
 })
 
 module.exports = {db}
