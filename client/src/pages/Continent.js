0ef6587a5f46262d1ebd71ffd183f43a0f3e6783
Kaciel Benitez <kacielbenitez@gmail.com>
2024-06-14 02:01:54 -0600

    p

--- client/src/pages/Continent.js
+++ client/src/pages/Continent.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import NavBar from '../navbar';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
