3e7646cc445a25618e2fb8f3e30e5dd14c4559b1
Kaciel Benitez <kacielbenitez@gmail.com>
2024-06-17 15:23:49 -0600

    v

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import App from './App';
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
 import { AuthenticationProvider } from './AuthProvider';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
